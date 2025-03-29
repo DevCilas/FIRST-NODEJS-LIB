f7a01dd57d4c4c92a1e23c1ba1709560a31047d9
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-03-28 23:30:26 -0300

    Feat - backlog

---
+++ src/index.js
@@ -0,0 +1,3 @@
+//*Receber arquivo a ser lido (Teste Require, depois process.argv) você vai usar os dois.
+
+//*Depois de conseguir adicionar o arquivo, você precisa lê-lo, para isso, pequise alguma biblioteca e função e leia suas documentação.
