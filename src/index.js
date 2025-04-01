5d2057bad7aebf4be136e297fb41ebf3245ab5e0
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-04-01 00:08:17 -0300

    Fix - File System

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-//*Receber arquivo a ser lido (Teste Require, depois process.argv) você vai usar os dois.
+const { log } = require('console');
+const fs = require('fs');
+//*FS é file system um módulo é usado para ler um arquivo dentro do sistema do compuatdor
+//*Além de ler o arqeuivo, podemos fazer update, deletar, criar e renomear.
+//*OBS: Uma biblioteca é um conjunto de modulos, modolus são códigos reutilizavéis
 
-//*Depois de conseguir adicionar o arquivo, você precisa lê-lo, para isso, pequise alguma biblioteca e função e leia suas documentação.
+//*Agora vamos passar o caminho do arquivo de forma que não fique dependendo de alterações manuais
+//*Para isso vamos usar a propriedade process.argv, ele retorna as repostas do terminal, sendo a primeira o interpretador, e depois o proório arquivo em questão.
+
+const filePath = process.argv;
+const link = filePath[2];
+//*Agora vamos usar o método do fs chamado READFILE.
+
+fs.readFile(link, 'utf-8', (err, data)=>{
+    if(err){
+        console.error('Erro no arquivo');
+        return
+    }
+
+    console.log(data)
+})
+
+
+
