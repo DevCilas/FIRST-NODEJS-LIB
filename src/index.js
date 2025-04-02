a013303b9df98964c762c2ec93f07c0f22f3dad7
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-04-02 10:40:31 -0300

    Fix - clearText function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { log } = require('console');
 const fs = require('fs');
+const { resourceUsage } = require('process');
 //*FS é file system um módulo é usado para ler um arquivo dentro do sistema do compuatdor
 //*Além de ler o arqeuivo, podemos fazer update, deletar, criar e renomear.
 //*OBS: Uma biblioteca é um conjunto de modulos, modolus são códigos reutilizavéis
@@ -17,8 +17,55 @@ fs.readFile(link, 'utf-8', (err, data)=>{
         return
     }
 
+<<<<<<< Updated upstream
     console.log(data)
+=======
+    splitIntoParagraphs(texto);
+>>>>>>> Stashed changes
 })
 
+function clearText(text){
+    text = text.toLowerCase().normalize();
+    text = text.replace(/[.,:;?!()]/g, '');
+    return text
 
+<<<<<<< Updated upstream
+=======
+}
+
+function splitIntoParagraphs(text){
+    // text = text.split('\n');
+    // return text
+    //*O código acima pdoe ser apenas:
+    text = text.split('\n');
+    console.log(text)
+}
+
+function separetedWords(text){
+    return text.split(' ');
+    
+}
+
+function createObject(array){
+    const paragraphs ={};
+    for (elemento of array){
+        if(!(elemento in paragraphs)){
+            paragraphs[elemento] = 1
+        }
+        else{
+            paragraphs[elemento] += 1
+        }
+    }
+    return paragraphs
+}
+
+function finalText(text){
+    text = clearText(text);
+    text = splitIntoParagraphs(text);
+    // text=separetedWords(text);
+    const result = createObject(text)
+    console.log(result);
+}
+
+>>>>>>> Stashed changes
 
